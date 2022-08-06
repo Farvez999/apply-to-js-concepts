e1418076dbef9b466f454a8dacd18af27b50df6d
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-06 22:08:05 +0600

    SumAllNumber& oddnumber array function

--- sumAllNumbersArray.js
+++ sumAllNumbersArray.js
@@ -1,13 +1,29 @@
-function findOddSum(numbers) {
+function findSum(numbers) {
     let sum = 0;
-    for (i = 0; i < numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         const index = i;
         const element = numbers[index];
         sum = sum + element;
-        console.log(index, element, sum);
+        //console.log(index, element, sum);
     }
     return sum;
 }
 
+function findOddNumberOfArrary(numbers) {
+    const oddNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element % 2 !== 0) {
+            console.log(index, element);
+            oddNumbers.push(element);
+        }
+    }
+    return oddNumbers;
+}
+
 const myNumber = [10, 36, 45, 59, 48, 60];
-findOddSum(myNumber);
+const oddNumbers = findOddNumberOfArrary(myNumber);
+console.log(oddNumbers);
+const findOddNumberSum = findSum(oddNumbers);
+console.log(findOddNumberSum);
